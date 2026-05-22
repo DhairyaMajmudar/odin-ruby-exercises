2eaa944e576a4999bb638030b0bc3d921f05f069
DhairyaMajmudar <dhairya.opensource@gmail.com>
2026-05-23 00:43:45 +0530

    add variables
    Signed-off-by: DhairyaMajmudar <dhairya.opensource@gmail.com>

---
+++ personal_learning_files/variables.rb
@@ -0,0 +1,8 @@
+# Ruby variables
+
+name = "Dhairya"
+age = 21
+
+# Variable names are reusable, so you can assign a new value to a variable at any point in your program. 
+# Meaning un ruby, variables are mutable.
+# IMP: Ruby evaluates the expression on the right first. 
