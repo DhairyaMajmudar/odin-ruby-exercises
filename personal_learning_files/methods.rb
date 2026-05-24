48cd14a525dc59b163f474931feafba04d5c7933
DhairyaMajmudar <dhairya.opensource@gmail.com>
2026-05-24 20:54:25 +0530

    methods
    Signed-off-by: DhairyaMajmudar <dhairya.opensource@gmail.com>

---
+++ personal_learning_files/methods.rb
@@ -0,0 +1,9 @@
+# Methods in Ruby
+# These are functions in js, but since in ruby everything is an object, we call them methods
+
+def greet(name)
+  "Hello, #{name}"
+end
+
+greet("Dhairya") # => "Hello, Dhairya"
+
